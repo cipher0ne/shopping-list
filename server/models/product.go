61cff3767fccc18ece3d1cc05abfdf6e8f1f6f78
cipher0ne <jakub.skrobot64@gmail.com>
2026-03-17 19:05:11 +0100

    Fix backend product model

--- server/models/product.go
+++ server/models/product.go
@@ -3,8 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
-	ID			primitive.ObjectID 	`json:"id.omitempty" bson:"_id.omitempty"`
+	ID			primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
 	Name		string				`json:"name" bson:"name"`
 	Quantity	int					`json:"quantity" bson:"quantity"`
-	Bought 		bool				`json:"Bought" bson:"Bought"`
+	Bought		bool				`json:"bought" bson:"bought"`
 }
